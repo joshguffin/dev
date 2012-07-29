cefa50c961a16efc2b178e67cc51cf08aded62a6
Josh Guffin <josh.guffin@gmail.com>
2012-07-28 20:32:33 -0400

    move integertostring test code to prog dir

--- library/string/integertostring.h
+++ library/string/integertostring.h
@@ -1,5 +1,5 @@
-#ifndef integertostring_h_INCLUDED
-#define integertostring_h_INCLUDED
+#ifndef string_integertostring_h_INCLUDED
+#define string_integertostring_h_INCLUDED
 
 #include <stdint.h>
 #include <assert.h>
@@ -11,13 +11,13 @@
 #include <boost/type_traits/is_signed.hpp>
 #include <boost/utility/enable_if.hpp>
 
-#include <boost/test/unit_test.hpp>
-
 namespace StringLib {
+
+namespace IntegerToStringImpl {
 
 // instantiated if the type is unsigned
 template <typename UnsignedType>
-typename boost::disable_if<typename boost::is_signed<UnsignedType>::type, char*>::type
+typename boost::disable_if<boost::is_signed<UnsignedType>, char*>::type
 FillBuffer(char* pt, UnsignedType input, char*buf)
 {
    BOOST_STATIC_ASSERT(std::numeric_limits<UnsignedType>::is_integer);
@@ -35,7 +35,7 @@ FillBuffer(char* pt, UnsignedType input, char*buf)
 
 // instantiated if the type is signed
 template <typename SignedType>
-typename boost::enable_if<typename boost::is_signed<SignedType>::type, char*>::type
+typename boost::enable_if<boost::is_signed<SignedType>, char*>::type
 FillBuffer(char* pt, SignedType input, char*buf)
 {
    BOOST_STATIC_ASSERT(std::numeric_limits<SignedType>::is_integer);
@@ -44,28 +44,20 @@ FillBuffer(char* pt, SignedType input, char*buf)
 
    bool negative = input < 0;
 
-   // handle input = MIN_INT
-   if (negative)
-   {
-      // sign of modulus of negative number is implementation-defined
-      *pt-- = std::abs(input % -10) + '0';
-      input /= -10;
-   }
+   UnsignedType uinput = negative ? -input : input;
 
-   while (input) {
-      assert(pt >= buf);
-      *pt-- = input % 10 + '0';
-      input /= 10;
-   }
+   pt = FillBuffer(pt, uinput, buf);
 
    if (negative)
-       *pt-- = '-';
+       *--pt = '-';
 
-   return ++pt;
+   return pt;
 }
 
+} // end of namespace IntegerToStringImpl
+
 template <typename IntegralType>
-std::string
+typename boost::disable_if<boost::is_enum<IntegralType>, std::string>::type
 IntegerToString(IntegralType input)
 {
    if (input == 0)
@@ -74,114 +66,26 @@ IntegerToString(IntegralType input)
    BOOST_STATIC_ASSERT(std::numeric_limits<IntegralType>::is_integer);
 
    const size_t bufferSize = (std::numeric_limits<IntegralType>::digits
-                              + static_cast<int>(boost::is_signed<IntegralType>::value));
+                              + static_cast<size_t>(boost::is_signed<IntegralType>::value));
 
    BOOST_STATIC_ASSERT(bufferSize);
 
    char buffer[bufferSize];
    char* end = buffer + bufferSize -1;
-   char* pt  = FillBuffer(end, input, buffer);
+   char* pt  = IntegerToStringImpl::FillBuffer(end, input, buffer);
 
    assert(pt >= buffer);
-   assert(pt <=  end);
+   assert(pt <= end);
    return std::string(pt, end - pt + 1);
 }
 
-// To run the unit tests, the following order of #includes is important.
-//
-// #include <iostream>
-// #include <sstream>
-//
-// #define BOOST_TEST_MAIN
-//
-// #include <boost/test/unit_test.hpp>
-// #include "string/integertostring.h"
-
-#ifdef BOOST_TEST_MAIN
-
-namespace IntegerToStringTest {
-
-template <typename T>
-std::string
-CreateStdString(T t)
+template <typename EnumType>
+typename boost::enable_if<boost::is_enum<EnumType>, std::string>::type
+IntegerToString(EnumType input)
 {
-   std::stringstream ss;
-   ss << t;
-   return ss.str();
+   return IntegerToString(static_cast<int>(input));
 }
-
-// stringstream needs to treat chars specially
-template <> std::string CreateStdString<signed char>(signed char t)
-{
-   std::stringstream ss;
-   ss << static_cast<int>(t);
-   return ss.str();
-}
-
-template <> std::string CreateStdString<unsigned char>(unsigned char t)
-{
-   std::stringstream ss;
-   ss << static_cast<uint>(t);
-   return ss.str();
-}
-
-template <typename T>
-void
-RunStringTest(T val)
-{
-   std::string libResult = StringLib::IntegerToString(val);
-   std::string stdResult = CreateStdString(val);
-   bool equal            = libResult == stdResult;
-
-   if (equal)
-      std::cout
-         << "\033[01;38;5;160m" << "OK" << "\033[39m" << " "
-         << libResult << "==" << stdResult << std::endl;
-   else
-      std::cout
-         << "lib: " << libResult << " " << "len=" << libResult.length() << '\n'
-         << "std: " << stdResult << " " << "len=" << stdResult.length() << std::endl;
-   BOOST_CHECK(equal);
-}
-
-#define CREATE_AUTO_TEST_CASE(T)                                                    \
-   BOOST_AUTO_TEST_CASE(IntegerToString_##T##_Test)                                 \
-   {                                                                                \
-      StringLib::IntegerToStringTest::RunStringTest(std::numeric_limits<T>::max()); \
-      StringLib::IntegerToStringTest::RunStringTest(std::numeric_limits<T>::min()); \
-      StringLib::IntegerToStringTest::RunStringTest(static_cast<T>(0));             \
-      StringLib::IntegerToStringTest::RunStringTest(static_cast<T>(1));             \
-      StringLib::IntegerToStringTest::RunStringTest(static_cast<T>(-1));            \
-   }
-
-CREATE_AUTO_TEST_CASE(int8_t   );
-CREATE_AUTO_TEST_CASE(int16_t  );
-CREATE_AUTO_TEST_CASE(int32_t  );
-CREATE_AUTO_TEST_CASE(int64_t  );
-CREATE_AUTO_TEST_CASE(u_int8_t );
-CREATE_AUTO_TEST_CASE(u_int16_t);
-CREATE_AUTO_TEST_CASE(u_int32_t);
-CREATE_AUTO_TEST_CASE(u_int64_t);
-
-#undef CREATE_AUTO_TEST_CASE
-
-BOOST_AUTO_TEST_CASE(IntegerToString_Sample_Values)
-{
-   StringLib::IntegerToStringTest::RunStringTest(true);
-   StringLib::IntegerToStringTest::RunStringTest(false);
-
-   StringLib::IntegerToStringTest::RunStringTest(static_cast<u_int16_t>(2355));
-   StringLib::IntegerToStringTest::RunStringTest(static_cast<int16_t>(-1295));
-
-   StringLib::IntegerToStringTest::RunStringTest(static_cast<int64_t>(-8372598273598235LL));
-
-
-}
-
-} // end of namespace IntegerToStringTest
-
-#endif // BOOST_TEST_MAIN
 
 } // end of namespace StringLib
 
-#endif // integertostring_h_INCLUDED
+#endif // string_integertostring_h_INCLUDED
--- prog/test/integertostring_test.cpp
+++ prog/test/integertostring_test.cpp
@@ -1,4 +1,3 @@
-// just compile this and link with -lboost_unit_test_framework
 #include <iostream>
 #include <sstream>
 
@@ -7,3 +6,99 @@
 #include <boost/test/unit_test.hpp>
 #include "string/integertostring.h"
 
+template <typename T>
+std::string
+CreateStdString(T t)
+{
+   std::stringstream ss;
+   ss << t;
+   return ss.str();
+}
+
+// stringstream needs to treat chars specially
+template <> std::string CreateStdString<signed char>(signed char t)
+{
+   std::stringstream ss;
+   ss << static_cast<int>(t);
+   return ss.str();
+}
+
+template <> std::string CreateStdString<unsigned char>(unsigned char t)
+{
+   std::stringstream ss;
+   ss << static_cast<uint>(t);
+   return ss.str();
+}
+
+template <typename T>
+void
+RunStringTest(T val, const std::string& description)
+{
+   std::string libResult = StringLib::IntegerToString(val);
+   std::string stdResult = CreateStdString(val);
+   bool equal            = libResult == stdResult;
+
+   if (equal)
+      std::cout
+         << "[\033[01;32;5;160m" << "OK" << "\033[0m]"
+         << " " << description
+         << " (" << libResult << " == " << stdResult << ")"
+         << std::endl;
+   else {
+      std::string errString = "[\033[01;38;5;160mERROR\033[0m]";
+      std::cout
+         << errString
+         << ' ' << '(' << description << ')'
+         << ' ' << "lib: " << libResult << " " << "len=" << libResult.length() << '\n'
+         << std::string(7, ' ')
+         << ' ' << "std: " << stdResult << " " << "len=" << stdResult.length() << std::endl;
+   }
+   BOOST_CHECK(equal);
+}
+
+#define CREATE_AUTO_TEST_CASE(T)                                                         \
+   BOOST_AUTO_TEST_CASE(IntegerToString_##T##_Test)                                      \
+   {                                                                                     \
+      StringLib::IntegerToStringTest::RunStringTest(std::numeric_limits<T>::max() , #T); \
+      StringLib::IntegerToStringTest::RunStringTest(std::numeric_limits<T>::min() , #T); \
+      StringLib::IntegerToStringTest::RunStringTest(static_cast<T>(0)             , #T); \
+      StringLib::IntegerToStringTest::RunStringTest(static_cast<T>(1)             , #T); \
+      StringLib::IntegerToStringTest::RunStringTest(static_cast<T>(-1)            , #T); \
+   }
+
+CREATE_AUTO_TEST_CASE(int8_t  );
+CREATE_AUTO_TEST_CASE(int16_t );
+CREATE_AUTO_TEST_CASE(int32_t );
+CREATE_AUTO_TEST_CASE(int64_t );
+CREATE_AUTO_TEST_CASE(uint8_t );
+CREATE_AUTO_TEST_CASE(uint16_t);
+CREATE_AUTO_TEST_CASE(uint32_t);
+CREATE_AUTO_TEST_CASE(uint64_t);
+
+#undef CREATE_AUTO_TEST_CASE
+
+#define RUN_EXPLICIT_TEST(Val) StringLib::IntegerToStringTest::RunStringTest(Val, #Val);
+
+   enum Unnamed    { One, Two };
+   enum Characters { charOne = 'a', charTwo = '\0', charThree = ' ' };
+   enum PosNeg     { someval=1, otherval=-1 };
+
+
+BOOST_AUTO_TEST_CASE(IntegerToString_Sample_Values)
+{
+   RUN_EXPLICIT_TEST(true);
+   RUN_EXPLICIT_TEST(false);
+   RUN_EXPLICIT_TEST(static_cast<uint16_t>(2355));
+   RUN_EXPLICIT_TEST(static_cast<int16_t>(-1295));
+   RUN_EXPLICIT_TEST(static_cast<int64_t>(-8372598273598235LL));
+
+
+   RUN_EXPLICIT_TEST(One);
+   RUN_EXPLICIT_TEST(Two);
+   RUN_EXPLICIT_TEST(charOne);
+   RUN_EXPLICIT_TEST(charTwo);
+   RUN_EXPLICIT_TEST(charThree);
+   RUN_EXPLICIT_TEST(someval);
+   RUN_EXPLICIT_TEST(otherval);
+
+}
