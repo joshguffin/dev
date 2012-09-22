e9251eb5ad8883ca659590cfd3786013d87b8274
Josh Guffin <josh.guffin@gmail.com>
2012-09-22 17:12:53 -0400

    Auto dependency generation, almost working

---
+++ library/include/common.h
@@ -0,0 +1,10 @@
+#ifndef include_common_h_INCLUDED
+#define include_common_h_INCLUDED
+
+// event logs
+#include <glog/include/glog/logging.h>
+#include <include/headermacros.h>
+
+
+#endif // include_common_h_INCLUDED
+
